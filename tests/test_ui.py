a3330cf36a3d9820e4e119d96abedc0564ca9aeb
Tom <justforji_ji@msn.com>
2025-07-04 10:38:10 +0800

    上傳yaml 實作排程

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -4,9 +4,6 @@ from config import load_data_file
 stock_list = load_data_file("test_targets.json")
 stock_number = stock_list[0]['symbol']
 
-# 取出全部的數字
-# stock_number_list = [d['symbol'] for d in stock_list]
-# print(stock_number_list)
 
 def test_get_stock_price():
     driver = create_chrome_driver()
